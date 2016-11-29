00a9cc65b1a99abb570cb8e0da4b50588935ff35
Kunal Verma <kunalverma@kunals-macbook-air.local>
2016-11-29 14:28:03 +0530

    Initial Commit

---
+++ KVCardSelectionVC/KVCardSelectionVC.h
@@ -0,0 +1,19 @@
+//
+//  KVCardSelectionVC.h
+//  KVCardSelectionVC
+//
+//  Created by Kunal Verma on 29/11/16.
+//  Copyright © 2016 Kunal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KVCardSelectionVC.
+FOUNDATION_EXPORT double KVCardSelectionVCVersionNumber;
+
+//! Project version string for KVCardSelectionVC.
+FOUNDATION_EXPORT const unsigned char KVCardSelectionVCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KVCardSelectionVC/PublicHeader.h>
+
+
